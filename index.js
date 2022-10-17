2d87092033aac121b2e153f35bb0c81be11425a6
kamlesh-21 <kumar2105k@gmail.com>
2022-10-17 23:48:03 +0530

    updated css and js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const modal = document.getElementById("modal")
-console.log(modal)
 const closeBtn = document.getElementById("modal-close-btn")
+const consentForm = document.getElementById("consent-form")
+const modalText = document.getElementById("modal-text")
+
 
 setTimeout(function(){
     modal.style.display = 'inline'
@@ -9,3 +11,17 @@ setTimeout(function(){
 closeBtn.addEventListener("click", function(){
     modal.style.display='none'
 })
+
+consentForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    modalText.innerHTML = `
+    <div class="modal-inner-loading">
+    <img src="images/loading.svg" class="loading">
+    <p id="uploadText">
+        Uploading your data to the dark web...
+    </p>
+    </div>
+    `
+})
+
+
