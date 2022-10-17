dde53ed84e724d995c2a23b4f8478745048d7377
kamlesh-21 <kumar2105k@gmail.com>
2022-10-17 19:54:31 +0530

    updated html css

---
+++ index.js
@@ -0,0 +1,7 @@
+const modal = document.getElementById("modal")
+console.log(modal)
+const closeBtn = document.getElementById("modal-close-btn")
+
+// setTimeout(fucntion(){
+//     modal.style.display = 'inline'
+// }, 1500)
